4e27cf31420e6b604046cf8752b515b2faf6ea89
Pradip Pawara <pradip@skele.co>
2026-09-12 19:22:49 +0530

    update

---
+++ src/app/robot.tsx
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/admin"],
+    },
+    sitemap: "https://skele-store.vercel.app/sitemap.xml",
+  };
+}
